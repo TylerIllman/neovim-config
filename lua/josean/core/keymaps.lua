ea1ba48031e615c8f4f3ab1a9adc0add4999a159
TylerIllman <tyler.illman.productions@gmail.com>
2024-05-11 17:34:38 +0800

    Add key map to swap split windows

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -15,6 +15,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sw", "<C-w>r", { desc = "Swap two split windows" }) -- Swap two split windows
 
 keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
