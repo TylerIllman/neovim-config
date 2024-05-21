dd4186d39a9393c938653852b61e732e9157d43d
TylerIllman <tyler.illman.productions@gmail.com>
2024-05-21 13:00:44 +0800

    commit prior to LG update

--- lua/josean/plugins/formatting.lua
+++ lua/josean/plugins/formatting.lua
@@ -6,6 +6,7 @@ return {
 
     conform.setup({
       formatters_by_ft = {
+        java = { "prettier" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
