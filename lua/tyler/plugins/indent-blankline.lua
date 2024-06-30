fd0ea25697525470928d2697c38c08f2a98044dd
TylerIllman <tyler.illman.productions@gmail.com>
2024-06-30 13:30:34 +0800

    Almost entirely back to normal (added cmp back)

--- lua/tyler/plugins/indent-blankline.lua
+++ lua/tyler/plugins/indent-blankline.lua
@@ -6,20 +6,20 @@ vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
-char_highlight_list = {
-  "IndentBlanklineIndent1",
-  "IndentBlanklineIndent2",
-  "IndentBlanklineIndent3",
-  "IndentBlanklineIndent4",
-  "IndentBlanklineIndent5",
-  "IndentBlanklineIndent6",
+local char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
 }
 
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
     opts = {
-      indent = { highlight = char_highlight_list},
-    }
+        indent = { highlight = char_highlight_list },
+    },
 }
