2079021b51624484db4f4f54b9b7709bdc84672e
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-09 16:45:53 +0800

    change vim auto save to happen on Insert leave

--- lua/tyler/plugins/vim-auto-save.lua
+++ lua/tyler/plugins/vim-auto-save.lua
@@ -1,11 +1,11 @@
 return {
-  "907th/vim-auto-save", -- Plugin repository
-  event = "InsertEnter", -- Load the plugin when entering insert mode
-  config = function()
-    -- Enable auto-save
-    vim.g.auto_save = 1 -- Activate auto-save
-    -- Additional options
-    vim.g.auto_save_silent = 1 -- Make auto-save silent
-    vim.g.auto_save_delay = 200 -- Delay in milliseconds before saving
-  end,
+    "907th/vim-auto-save", -- Plugin repository
+    event = "InsertLeave", -- Load the plugin when entering insert mode
+    config = function()
+        -- Enable auto-save
+        vim.g.auto_save = 1 -- Activate auto-save
+        -- Additional options
+        vim.g.auto_save_silent = 1 -- Make auto-save silent
+        vim.g.auto_save_delay = 200 -- Delay in milliseconds before saving
+    end,
 }
